21f7076725adaab0ce7ee94682515037e6a74ad9
Navaneeth S Raju <navaneethsraju@gmail.com>
2026-08-25 21:47:44 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+
+const question = item.querySelector(".faq-question");
+const answer = item.querySelector(".faq-answer");
+
+question.addEventListener("click", () => {
+
+    const isOpen = item.classList.contains("active");
+
+    // Close all FAQ items
+    faqItems.forEach((faq) => {
+    faq.classList.remove("active");
+    faq.querySelector(".faq-answer").style.maxHeight = null;
+    });
+
+    // Open clicked FAQ
+    if (!isOpen) {
+    item.classList.add("active");
+    answer.style.maxHeight = answer.scrollHeight + "px";
+    }
+
+});
+
+});
+
 const form = document.getElementById("inquiryForm");
 
 const firstName = document.getElementById("firstName");
